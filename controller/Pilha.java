2c3fe5b2a7b222d5b18bc5087b0b2f52b0496aea
geoliveira <38046853+geoliveira@users.noreply.github.com>
2019-06-30 17:59:38 -0300

    comentando a classe

--- controller/Pilha.java
+++ controller/Pilha.java
@@ -1,25 +1,26 @@
 package controller;
 import model.*;
 
+// implementamos uma pilha de tam. fixo 3 por motivos de desempilharmos, formando fórmulas com o conectivo principal sendo unário ou binário
 public class Pilha {
 	Formula[] pilha = new Formula[3];
 	int topo = -1;
-
+	// tamanho
 	int size() {
 		if ( empty() ) return 0;
 		return topo+1;
 	}
-
+	// pilha vazia
 	boolean empty() {
 		if ( topo == -1 ) return true;
 		return false;
 	}
-
+	// pilha cheia
 	boolean flow() {
 		if ( topo == 2 ) return true;
 		return false;
 	}
-	
+	// empilha
 	void push(Formula f) {
 		if (flow()) {
 			System.out.println("Error: overflow");
@@ -30,7 +31,7 @@ public class Pilha {
 			pilha[topo] = f;
 		}
 	}
-
+	// desempilha
 	Formula pop() {
 		if (empty()){
 			System.out.println("Error: underflow");
@@ -45,13 +46,32 @@ public class Pilha {
 
 		return x;
 	}
-
+	// método para vermos o que há no topo da pilha (foi usado somente para debugar)
 	void top() {
 		if (empty()) System.out.println("Error: stack empty");
 
 		System.out.println(pilha[topo]);
 	}
+	// as funções abaixo de nome ready são essenciais para as funções de reduceStack, pois são conduções fundamentais para formação das subformulas
+	boolean readyUnary() {
+		if ( pilha[topo] instanceof Negacao && size() == 2 ) return true;
+		return false;
+	}
+	
+	boolean readyBinary() {
+		if (flow()) return true;
+		return false;
+	}
+	// verifica se o topo da pilha é do tipo Atomica
+	boolean isAtomica() {
+		if (empty()) System.out.println("Error: stack empty");
 
+		if ( pilha[topo] instanceof Atomica ) return true;
+
+		return false;
+	}
+	// obs (métodos reduceStack): a cada posição da pilha verificamos as possibilidades do tipo do elemento, exeto na posição que estará o elemento de tipo Conectivo, que será a raiz da árvore, na perspectiva do composite.
+	// reduzindo o tam. da pilha e criando uma fórmula com o conectivo principal (no raiz na arv. do composite) sendo unário
 	void reduceStackUnary() {
 		Conectivo conecUn = (Conectivo)pop();
 
@@ -62,7 +82,7 @@ public class Pilha {
 
 		push(conecUn);
 	}
-	
+	// reduzindo o tam. da pilha e criando uma fórmula com o conectivo principal (no raiz na arv. do composite) sendo binário
 	void reduceStackBinary() {
 		Atomica atTop = null, atLow = null;
 		Conectivo conecTop = null, conecCenter = null, conecLow = null;
@@ -91,23 +111,4 @@ public class Pilha {
 		
 		push(conecCenter);
 	}
-
-	boolean readyUnary() {
-		if ( pilha[topo] instanceof Negacao && size() == 2 ) return true;
-		return false;
-	}
-	
-	boolean readyBinary() {
-		if (flow()) return true;
-		return false;
-	}
-
-	boolean isAtomica() {
-		if (empty()) System.out.println("Error: stack empty");
-
-		if ( pilha[topo] instanceof Atomica ) return true;
-
-		return false;
-	}
 }
-
