1e7e3374bc8ef1e293240ffd5593390968f8ec82
Ubiratan <r14.ubiratanjunior@gmail.com>
2022-02-09 21:36:48 -0300

    Incrementando testes

--- src/test/java/controller/LeitorTest.java
+++ src/test/java/controller/LeitorTest.java
@@ -5,66 +5,58 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import model.Atomica;
+import model.Conectivo;
+import model.Conjuncao;
+import model.Disjuncao;
 import model.Formula;
 
 class LeitorTest {
 
-	
-	@Test
-	void testCriarFormulaAtomica() {
-		Formula formulaAtomica = new Atomica("A");
-		assertEquals(false, formulaAtomica.obterValor());
-	}
+    @Test
+    void testCriarFormulaAtomica() {
+	Formula formulaAtomica = new Atomica("A");
+	assertEquals(false, formulaAtomica.obterValor());
+    }
+    
 
-	
-	@Test
-	void testFimSubFormulaSimplesSemNegacao() {
-		String formula = "A+B(C+D)";
-		
-		Leitor leitor = new Leitor(formula);
-		
-		int fim = leitor.fimSubFormula(leitor.getArrayPsi(), 2);
-		assertEquals(formula.indexOf(")"), fim);
-		
-	}
+    @Test
+    void testFimSubFormulaSimplesSemNegacao() {
+	String formula = "A+B(C+D)";
+	Leitor leitor = new Leitor(formula);
 
-	@Test
-	void testFimSubFormulaSimplesComNegacao() {
-		String formula = "A+B(C+D)'";
-		
-		Leitor leitor = new Leitor(formula);
-		
-		int fim = leitor.fimSubFormula(leitor.getArrayPsi(), 2);
-		assertEquals(formula.indexOf("'"), fim);
-		
-	}
-	
-	
-	@Test
-	void testFimSubFormulaCompostaSemNegacao() {
-		String formula = "A+B(C+(D.E))";
-		
-		Leitor leitor = new Leitor(formula);
-		
-		int fim = leitor.fimSubFormula(leitor.getArrayPsi(), 4);
-		assertEquals(formula.indexOf(")"), fim);
-		
-	}
+	int fim = leitor.fimSubFormula(leitor.getArrayPsi(), 2);
+	assertEquals(formula.indexOf(")"), fim);
 
-	@Test
-	void testFimSubFormulaCompostaComNegacao() {
-		String formula = "A+B((C+D)')";
-		
-		Leitor leitor = new Leitor(formula);
-		
-		int fim = leitor.fimSubFormula(leitor.getArrayPsi(), 4);
-		assertEquals(formula.indexOf("'"), fim);
-		
-	}
-//
-//	@Test
-//	void testLerFormula() {
-//		fail("Not yet implemented");
-//	}
+    }
 
+    @Test
+    void testFimSubFormulaSimplesComNegacao() {
+	String formula = "A+B(C+D)'";
+	Leitor leitor = new Leitor(formula);
+
+	int fim = leitor.fimSubFormula(leitor.getArrayPsi(), 2);
+	assertEquals(formula.indexOf("'"), fim);
+
+    }
+
+    @Test
+    void testFimSubFormulaCompostaSemNegacao() {
+	String formula = "A+B(C+(D.E))";
+	Leitor leitor = new Leitor(formula);
+
+	int fim = leitor.fimSubFormula(leitor.getArrayPsi(), 4);
+	assertEquals(formula.indexOf(")"), fim);
+
+    }
+
+    @Test
+    void testFimSubFormulaCompostaComNegacao() {
+	String formula = "A+B((C+D)')";
+
+	Leitor leitor = new Leitor(formula);
+
+	int fim = leitor.fimSubFormula(leitor.getArrayPsi(), 4);
+	assertEquals(formula.indexOf("'"), fim);
+
+    }
 }
