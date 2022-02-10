77e8ffcff67f289faa1d26e7ef901202081b4e2a
Ubiratan <r14.ubiratanjunior@gmail.com>
2022-02-10 09:53:15 -0300

    Atualizando pom e classes pra teste

--- src/test/java/model/ConjuncaoTest.java
+++ src/test/java/model/ConjuncaoTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ConjuncaoTest {
+public class ConjuncaoTest {
 
 	private Atomica A;
 	private Atomica B;
